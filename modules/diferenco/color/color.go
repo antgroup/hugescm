b8510ba82d17c11b20575bf144d6aff3b6aa9cca
J23 <charlieio@outlook.com>
2024-12-13 23:18:51 +0800

    diff: Better ANSI color compatibility

--- modules/diferenco/color/color.go
+++ modules/diferenco/color/color.go
@@ -1,37 +1,49 @@
 package color
 
 // TODO read colors from a github.com/go-git/go-git/plumbing/format/config.Config struct
-// TODO implement color parsing, see https://github.com/git/git/blob/v2.26.2/color.c
+// TODO implement color parsing, see https://github.com/git/git/blob/v2.47.1/color.c
 
-// Colors. See https://github.com/git/git/blob/v2.26.2/color.h#L24-L53.
+// Colors. See https://github.com/git/git/blob/v2.47.1/color.h#L25-L66.
 const (
 	Normal       = ""
-	Reset        = "\033[m"
+	Reset        = "\033[0m"
 	Bold         = "\033[1m"
+	Black        = "\033[30m"
 	Red          = "\033[31m"
 	Green        = "\033[32m"
 	Yellow       = "\033[33m"
 	Blue         = "\033[34m"
 	Magenta      = "\033[35m"
 	Cyan         = "\033[36m"
+	White        = "\033[37m"
+	Default      = "\033[39m"
+	BoldBlack    = "\033[1;30m"
 	BoldRed      = "\033[1;31m"
 	BoldGreen    = "\033[1;32m"
 	BoldYellow   = "\033[1;33m"
 	BoldBlue     = "\033[1;34m"
 	BoldMagenta  = "\033[1;35m"
 	BoldCyan     = "\033[1;36m"
+	BoldWhite    = "\033[1;37m"
+	BoldDefault  = "\033[1;39m"
+	FaintBlack   = "\033[2;30m"
 	FaintRed     = "\033[2;31m"
 	FaintGreen   = "\033[2;32m"
 	FaintYellow  = "\033[2;33m"
 	FaintBlue    = "\033[2;34m"
 	FaintMagenta = "\033[2;35m"
 	FaintCyan    = "\033[2;36m"
+	FaintWhite   = "\033[2;37m"
+	FaintDefault = "\033[2;39m"
+	BgBlack      = "\033[40m"
 	BgRed        = "\033[41m"
 	BgGreen      = "\033[42m"
 	BgYellow     = "\033[43m"
 	BgBlue       = "\033[44m"
 	BgMagenta    = "\033[45m"
 	BgCyan       = "\033[46m"
+	BgWhilte     = "\033[47m"
+	BgDefault    = "\033[49m"
 	Faint        = "\033[2m"
 	FaintItalic  = "\033[2;3m"
 	Reverse      = "\033[7m"

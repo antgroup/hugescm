0f20b4737ced2eed070e44edef8dbd81f7974fb5
LBW <charlieio@outlook.com>
2025-12-08 11:39:00 +0800

    histogram diff testcase

--- modules/diferenco/histogram_test.go
+++ modules/diferenco/histogram_test.go
@@ -36,3 +36,92 @@ func TestHistogram(t *testing.T) {
 	e.SetColor(color.NewColorConfig())
 	_ = e.Encode([]*Unified{u})
 }
+
+func TestHistogram2(t *testing.T) {
+	lines1 := `A
+x
+A
+A
+A
+x
+A
+A
+A`
+	lines2 := `A
+x
+A
+Z
+A
+x
+A
+A
+A`
+	sink := &Sink{
+		Index: make(map[string]int),
+	}
+	a := sink.SplitLines(lines1)
+	b := sink.SplitLines(lines2)
+	changes, _ := HistogramDiff(t.Context(), a, b)
+	u := sink.ToUnified(&File{Name: "a.txt"}, &File{Name: "b.txt"}, changes, a, b, DefaultContextLines)
+	e := NewUnifiedEncoder(os.Stderr)
+	e.SetColor(color.NewColorConfig())
+	_ = e.Encode([]*Unified{u})
+}
+
+func TestHistogram3(t *testing.T) {
+	lines1 := `a
+b
+c
+a
+b
+c`
+	lines2 := `x
+ b
+z
+a
+b
+c`
+	sink := &Sink{
+		Index: make(map[string]int),
+	}
+	a := sink.SplitLines(lines1)
+	b := sink.SplitLines(lines2)
+	changes, _ := HistogramDiff(t.Context(), a, b)
+	u := sink.ToUnified(&File{Name: "a.txt"}, &File{Name: "b.txt"}, changes, a, b, DefaultContextLines)
+	e := NewUnifiedEncoder(os.Stderr)
+	e.SetColor(color.NewColorConfig())
+	_ = e.Encode([]*Unified{u})
+}
+
+func TestHistogram4(t *testing.T) {
+	lines1 := `a
+b
+c
+a
+b
+c
+a
+b
+c`
+	lines2 := `a
+b
+c
+a1
+a2
+a3
+b
+c1
+a
+b
+c`
+	sink := &Sink{
+		Index: make(map[string]int),
+	}
+	a := sink.SplitLines(lines1)
+	b := sink.SplitLines(lines2)
+	changes, _ := HistogramDiff(t.Context(), a, b)
+	u := sink.ToUnified(&File{Name: "a.txt"}, &File{Name: "b.txt"}, changes, a, b, DefaultContextLines)
+	e := NewUnifiedEncoder(os.Stderr)
+	e.SetColor(color.NewColorConfig())
+	_ = e.Encode([]*Unified{u})
+}
